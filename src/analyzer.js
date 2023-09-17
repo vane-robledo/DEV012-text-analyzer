b0bfe9ca9b48b567b902498c84e00e63bc58398b
vanelopezr <vanne.lopez.r@gmail.com>
2023-09-17 00:11:00 -0300

    funciones casi, sin css

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,69 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
+    text = text.trim();
+    if (text === "" || !/[a-zA-Z0-9]/.test(text)) {
+      return 0;
+    }
+    const words = text.split(/\s+/);
+    return words.length;
+    //return palabras.length;
+
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
+
+    return text.length;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+    if (text === "") {
+      return 0;
+    } else {
+      const characters = text.match(/[a-zA-Z0-9]/g);
+
+      if (characters === null) {
+        return 0;
+      }
+      return characters.length;
+    }
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getAverageWordLength: (text) => {
+
+    if (!text.match(/[a-zA-Z0-9]/)) {
+      return 0
+    }
+    const words = text.split(/\s+/); //arreglo con palabras separadas de a una
+    let lengthSum = 0;
+
+    for (let i = 0; i < words.length; i++) {
+      const length = words.length;
+      lengthSum += length;
+    }
+    const averageLength = lengthSum / words.length;
+    return parseFloat(averageLength.toFixed(2))
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.match(/\b\d+(\.\d+)?\b/g)
+    if (numbers === null) {
+      return 0;
+    } else {
+      return numbers.length;
+      //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    }
   },
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
-};
+    const numbers = text.match(/\b\d+(\.\d+)?\b/g);
+    let sum = 0;
+    if (numbers === null) {
+      return 0;
+    }
+    for (let i = 0; i < numbers.length; i++) {
+      sum += parseFloat(numbers[i]);
+    }
+    return sum
+  }
 
+}
+  ;
 export default analyzer;
--- test/analyzer.spec.js
+++ test/analyzer.spec.js
@@ -68,7 +68,7 @@ describe('analyzer', () => {
 });  
 
 //TODO: Remove .skip to execute the test for optional functionalities.
-describe.skip('Optional:', () => {
+describe('Optional:', () => {
   const TEST_TEST_EMPTY = '';
   const TEST_TEST_SPACES = '       ';
   const TEST_TEXT_PUNCTUATION_MARKS = '.,;:"«»[]{}()¿?¡!-';
